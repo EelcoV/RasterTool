f1e7e849d85173d48a5bde1d5fc4481d9c4c651d
Eelco Vriezekolk <eelco@Computer-Eelco.local>
2014-11-26 20:12:41 +0100

    Added a Target to create a distribution packages, and updated the Release Notes so that some more useful information is included in it. Create a branch in which upgrades to the latest versions of JQuery, JQuery-UI, and jsPlumb are worked out.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+distribution=`date +%Y%m%d_%H%M`
+
+files="\
+index.inc share.inc manual-0.9.1.pdf index.html \
+help/*.html \
+img/*.png img/blue/*.png img/green/*.png img/grey/*.png img/none/*.png img/orange/*.png img/purple/*.png img/red/*.png img/yellow/*.png \
+public_group/htaccess public_group/*.php \
+css/*.css css/*/*.css css/*/images \
+js/encodedecode.js js/eventsource.js js/jquery.localisation.js js/ui.tabs.closable.js js/jquery.json-2.2.min.js \
+js/jquery.editinplace-2.2.1.js \
+js/jquery-1.7.2.js js/jquery-ui-1.8.24.js js/jquery.jsPlumb-1.5.5.js \
+js/rasterMain.js js/rasterComponent.js js/rasterNode.js js/rasterNodeCluster.js \
+js/rasterPreferencesObject.js js/rasterProject.js js/rasterService.js js/rasterThreatEvaluation.js \
+js/translation-nl.js \
+ReleaseNotes.txt \
+"
+
+export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
+export COPYFILE_DISABLE=true
+
+tar -c --exclude '._' -z -f Raster-$distribution.tar.gz $files
