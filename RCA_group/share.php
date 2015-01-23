1849cac04cc434957ae123a9d5d0aedfbc7fbef1
Eelco Vriezekolk <eelcov@xs4all.nl>
2015-01-23 19:45:28 +0100

    Deleted unused and unnecessary files.

--- RCA_group/share.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* The name of the current directory is the name of this access group */
-define("GROUP", end(explode(DIRECTORY_SEPARATOR,dirname(__FILE__))) );
-
-require "../share.inc";
-?>
