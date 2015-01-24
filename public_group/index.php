4128cea6f046790ae36808c38723af743222dfab
Eelco Vriezekolk <eelcov@xs4all.nl>
2015-01-24 16:51:18 +0100

    Fixed incorrect license notes in PHP code.

--- public_group/index.php
+++ public_group/index.php
@@ -1,9 +1,9 @@
+<?php
 /*
 Copyright (C) Eelco Vriezekolk, Universiteit Twente, Agentschap Telecom.
 See LICENSE.md
 */
 
-<?php
 /* The name of the current directory is the name of this access group */
 define("GROUP", end(explode(DIRECTORY_SEPARATOR,dirname(__FILE__))) );
 
