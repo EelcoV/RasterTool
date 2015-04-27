f2902803131dca2b392a0df38553a8e4190dddd5
Eelco Vriezekolk <eelcov@xs4all.nl>
2015-04-27 11:33:54 +0200

    Update 'about' texts. Small correction to build.sh.

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ files="\
 index.inc share.inc Raster-EN-1.0.pdf Raster-NL-1.0.pdf index.html \
 help/*.html \
 img/*.png img/blue/*.png img/green/*.png img/grey/*.png img/none/*.png img/orange/*.png img/purple/*.png img/red/*.png img/yellow/*.png \
-public_group/htaccess public_group/*.php \
+public_group/htaccess public_group/*.php public_group/SharedProjects/README.md \
 css/*.css css/*/*.css css/*/images \
 js/encodedecode.js js/eventsource.js js/jquery.localisation.js js/jquery.editinplace-2.2.1.js \
 js/jquery-2.1.3.js js/jquery-ui-1.11.2.js js/jquery.jsPlumb-1.7.5.js \
