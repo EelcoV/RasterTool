637a96980936c38663c8b74c2d986f1eaeb9fd6c
Eelco Vriezekolk <eelcov@xs4ll.nl>
2017-09-29 13:12:03 +0200

    Changed sse_projmon to avoid buffering issues on the server, and updated to avoid PHP strict errors.

--- public_group/sse_projmon.php
+++ public_group/sse_projmon.php
@@ -4,8 +4,11 @@ Copyright (C) Eelco Vriezekolk, Universiteit Twente, Agentschap Telecom.
 See LICENSE.md
 */
 
+/* The end() function needs a variable */
+$dummy = explode(DIRECTORY_SEPARATOR,dirname(__FILE__));
+
 /* The name of the current directory is the name of this access group */
-define("GROUP", end(explode(DIRECTORY_SEPARATOR,dirname(__FILE__))) );
+define("GROUP", end($dummy) );
 
 require "../sse_projmon.inc";
 ?>
