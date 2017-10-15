e8cc5132a2476a7eefd3229226243a429a204f9c
Eelco Vriezekolk <eelco@computer-eelco.local>
2017-10-15 19:07:12 +0200

    Also build server version from the sources.

--- script/preprocess-server.sh
+++ script/preprocess-server.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 
-for f in raw/raster*.js
+BUILDDIR=build/server
+echo "Building server version $BUILDDIR..."
+
+if [ ! -d $BUILDDIR ]; then
+	mkdir -p $BUILDDIR
+fi
+
+# Fixed sources
+cp -R -p server/* $BUILDDIR
+cp -R -p common/* $BUILDDIR
+cp -R -p public_group $BUILDDIR
+
+for srcfile in src/raster*.js
 do
-	cpp -E -P -C -w -DSERVER $f `basename ${f}`
+	destfile=$BUILDDIR/js/`basename $srcfile`
+	if [ $srcfile -nt $destfile ]; then
+		cpp -E -P -C -w -DSERVER $srcfile $destfile
+		# Check whether the sources are correct, and correctly preprocessed
+		script/lint/jsl -nologo -nosummary -conf script/lint/jsl.default.conf -process "$destfile" || exit 1
+	fi
 done
 
-# Check whether the sources are correct, and correctly preprocessed
-for f in raster*.js translation*.js
-do
-	lint/jsl -nologo -nosummary -conf lint/jsl.default.conf -process "$f" || exit 1
-done
+srcfile=src/index.inc
+destfile=$BUILDDIR/index.inc
+if [ $srcfile -nt $destfile ]; then
+	cpp -E -P -C -w -DSERVER $srcfile $destfile
+fi
 
+echo "...done"
