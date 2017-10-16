f22617d10b6c6a30d043e18be2cb8bc61d9e39c9
Eelco Vriezekolk <eelco@computer-eelco.local>
2017-10-16 22:14:15 +0200

    First attempt at Win32 installation.

--- script/build-electron-macos.sh
+++ script/build-electron-macos.sh
@@ -2,7 +2,7 @@
 
 VERSION=1.7.9
 
-CreateLanguageVersion()
+CreateMacOSLanguageVersion()
 {
 	LANG=$1
 	BUILDDIR=build/standalone/$LANG
@@ -36,18 +36,56 @@ CreateLanguageVersion()
 	cp -p standalone/* $APPDIR
 	#mv $APPDIR/standalone.html $APPDIR/index.html
 
-	(
-		cd build
-		cp ../base.EN.dmg temp.dmg
-		hdiutil attach temp.dmg
-		cp -p -R $BASEDIR/Electron.app "/Volumes/Raster EN"
-		hdiutil detach "/Volumes/Raster EN"
-		hdiutil convert temp.dmg -format UDRO -o Raster.$LANG.dmg
-		rm -f temp.dmg
-	)
+	cp base.EN.dmg build/temp.dmg
+	hdiutil attach build/temp.dmg
+	cp -R -p $BASEDIR/Electron.app "/Volumes/Raster EN"
+	hdiutil detach "/Volumes/Raster EN"
+	rm -f build/Raster.$LANG.dmg
+	hdiutil convert build/temp.dmg -format UDRO -o build/Raster.$LANG.dmg
+	rm -f build/temp.dmg
 
 	echo "...done."
 }
 
-CreateLanguageVersion "EN"
-CreateLanguageVersion "NL"
+CreateWin32LanguageVersion()
+{
+	LANG=$1
+	BUILDDIR=build/standalone/$LANG
+	BASEDIR=build/electron-v$VERSION-win32-ia32-$LANG
+	APPDIR=$BASEDIR/resources/app
+
+	echo "Building $LANG version for Win32..."
+
+	script/lint/jsl -nologo -nosummary -conf script/lint/jsl.default.conf -process standalone/main.js || exit 1
+
+	if [ ! -d $BUILDDIR ]; then
+		echo "Build directory $BUILDDIR does not exist." && exit 1
+	fi
+
+	if [ -d $BASEDIR ]
+	then
+		rm -fr $APPDIR
+	else
+		mkdir -p $BASEDIR
+		( cd $BASEDIR && unzip ../../electron-v$VERSION-win32-ia32.zip )
+	fi
+
+	mkdir $APPDIR
+	mkdir $APPDIR/js
+	mkdir $APPDIR/img
+	mkdir $APPDIR/help
+	mkdir $APPDIR/app
+
+	cp -R -p $BUILDDIR/* $APPDIR
+	cp -p standalone/* $APPDIR
+	#mv $APPDIR/standalone.html $APPDIR/index.html
+
+	echo "...done."
+}
+
+CreateMacOSLanguageVersion "EN"
+CreateMacOSLanguageVersion "NL"
+
+CreateWin32LanguageVersion "EN"
+CreateWin32LanguageVersion "NL"
+
