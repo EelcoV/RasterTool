6e992e51ab20fc42e214257e17d77f2db5b76661
Eelco Vriezekolk <eelco@computer-eelco.local>
2017-10-17 21:52:29 +0200

    May prevent hdiutil detach from not ejecting DMG file.

--- script/build-electron-macos.sh
+++ script/build-electron-macos.sh
@@ -86,6 +86,7 @@ CreateMacOSVersion()
 	cp base.$LANG.dmg build/temp.dmg
 	hdiutil attach build/temp.dmg
 	cp -R -p $BASEDIR/Electron.app "/Volumes/Raster $LANG"
+	sync && sync
 	hdiutil detach "/Volumes/Raster $LANG"
 	rm -f build/Raster.$LANG.dmg
 	hdiutil convert build/temp.dmg -format UDRO -o build/Raster.$LANG.dmg
