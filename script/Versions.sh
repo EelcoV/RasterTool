5994901a0f08880078f26462dd9a23b0c3ca2e37
Eelco Vriezekolk <eelcov@xs4all.nl>
2018-02-09 09:55:25 +0100

    Prepare for release as version 2.1.0.

--- script/Versions.sh
+++ script/Versions.sh
@@ -3,9 +3,9 @@
 #
 
 ELECTRONVERSION="1.8.2"
-RASTERNUMVERSION="2.1b"
-RASTERVERSION="2.1 beta"
-RASTERSEASON="Beta version"
+RASTERNUMVERSION="2.1.0"
+RASTERVERSION="2.1"
+RASTERSEASON="Start 2018"
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # !!! When changing the version, also change Raster.EN.nsis and Raster.NL.nsis !!
