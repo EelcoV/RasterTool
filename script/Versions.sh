ef826618c61d44499a972d895ecff3cf4b7840e5
Eelco Vriezekolk <eelcov@xs4all.nl>
2018-02-16 11:36:26 +0100

    Update version strings.

--- script/Versions.sh
+++ script/Versions.sh
@@ -4,8 +4,8 @@
 
 ELECTRONVERSION="1.8.2"
 RASTERNUMVERSION="2.2.0b"
-RASTERVERSION="2.1 beta"
-RASTERSEASON="Start 2018"
+RASTERVERSION="2.2 beta"
+RASTERSEASON="Spring 2018"
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # !!! When changing the version, also change Raster.EN.nsis and Raster.NL.nsis !!
