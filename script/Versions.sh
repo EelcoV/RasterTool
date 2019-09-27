83bebcced126ca08feac38223aa9433724a741d9
Eelco Vriezekolk <eelcov@xs4all.nl>
2019-09-27 21:07:01 +0200

    Update Electron from 2.0.1 to 6.0.10.

--- script/Versions.sh
+++ script/Versions.sh
@@ -2,10 +2,10 @@
 # Common settings for all builds
 #
 
-ELECTRONVERSION="2.0.1"
-RASTERNUMVERSION="2.2.1"
-RASTERVERSION="2.2.1"
-RASTERSEASON="Winter 2018"
+ELECTRONVERSION="6.0.10"
+RASTERNUMVERSION="2.2.2b"
+RASTERVERSION="2.2.2 beta"
+RASTERSEASON="Winter 2019"
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # !!! When changing the version, also change Raster.EN.nsis and Raster.NL.nsis !!
