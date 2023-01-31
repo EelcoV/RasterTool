8d1e7d3c2c1b40b6fcdddd96d311174c4347341a
Eelco Vriezekolk <eelco@ztpe.nl>
2023-01-31 21:41:56 +0100

    Documentation updates.

--- script/Versions.sh
+++ script/Versions.sh
@@ -5,7 +5,7 @@
 ELECTRONVERSION="22.1.0"
 RASTERNUMVERSION="4.0beta"
 RASTERVERSION="4.0 beta"
-RASTERSEASON="Winter 2022"
+RASTERSEASON="Winter 2023"
 
 # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 # !!! When changing the version, also change Raster.EN.nsis and Raster.NL.nsis !!
